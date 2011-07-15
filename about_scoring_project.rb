76724cbfae68ab990be1c693b99f895b76845126
Javier Julio <JJfutbol@gmail.com>
2011-07-15 02:57:08 -0400

    Completed scoring project! Thought this one would be a bit tough but first idea came through and was able to pass all tests. Trick is to capture the number of times found for each unique die value. I enjoyed this one!

--- about_scoring_project.rb
+++ about_scoring_project.rb
@@ -30,7 +30,47 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 # Your goal is to write the score method.
 
 def score(dice)
-  # You need to write this method
+  result = 0
+  
+  # First idea: loop through all values and add to hash but only 
+  # once per unique value and have a counter for how many times 
+  # value was found
+  
+  # The param for the new() method call acts as a default value if 
+  # an accessed key doesn't exist. Awesome! Saved me from writing a 
+  # bloated each statement with an if/elsif to handle existence!
+  counts = Hash.new(0) 
+  
+  dice.each do |value|
+    counts[value] += 1
+  end
+  
+  counts.each do |item,numFound|
+    # 1,1,1 = 1000 points
+    if item == 1 && numFound >= 3 then
+      result += 1000
+      numFound -= 3
+    end
+    
+    # any number other than 1, found 3 times is that number times 
+    # 100.. so 5,5,5 = 500 points, 3,3,3 = 300 points, etc.
+    if item != 1 && numFound >= 3 then
+      result += item * 100
+      numFound -= 3
+    end
+    
+    # 1 (not part of set) = 100 points for each found
+    if item == 1 && numFound <= 2 then 
+      result += 100 * numFound
+    end
+    
+    # 5 (not part of set) = 50 points for each found
+    if item == 5 && numFound <=2 then
+      result += 50 * numFound
+    end
+  end
+  
+  result
 end
 
 class AboutScoringProject < EdgeCase::Koan
