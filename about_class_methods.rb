6f24aacab84fde113190f0612a17c1d2722881a3
Javier Julio <JJfutbol@gmail.com>
2011-08-11 19:47:31 -0400

    Completed class methods tests

--- about_class_methods.rb
+++ about_class_methods.rb
@@ -6,24 +6,24 @@ class AboutClassMethods < EdgeCase::Koan
 
   def test_objects_are_objects
     fido = Dog.new
-    assert_equal __, fido.is_a?(Object)
+    assert_equal true, fido.is_a?(Object)
   end
 
   def test_classes_are_classes
-    assert_equal __, Dog.is_a?(Class)
+    assert_equal true, Dog.is_a?(Class)
   end
 
   def test_classes_are_objects_too
-    assert_equal __, Dog.is_a?(Object)
+    assert_equal true, Dog.is_a?(Object)
   end
 
   def test_objects_have_methods
     fido = Dog.new
-    assert fido.methods.size > _n_
+    assert fido.methods.size > 0
   end
 
   def test_classes_have_methods
-    assert Dog.methods.size > _n_
+    assert Dog.methods.size > 0
   end
 
   def test_you_can_define_methods_on_individual_objects
@@ -31,7 +31,7 @@ class AboutClassMethods < EdgeCase::Koan
     def fido.wag
       :fidos_wag
     end
-    assert_equal __, fido.wag
+    assert_equal :fidos_wag, fido.wag
   end
 
   def test_other_objects_are_not_affected_by_these_singleton_methods
@@ -40,8 +40,10 @@ class AboutClassMethods < EdgeCase::Koan
     def fido.wag
       :fidos_wag
     end
-
-    assert_raise(___) do
+    
+    # The wag method was only added to the fido instance of the Dog 
+    # class. The rover instance was not given that method.
+    assert_raise(NoMethodError) do
       rover.wag
     end
   end
@@ -59,13 +61,13 @@ class AboutClassMethods < EdgeCase::Koan
   end
 
   def test_since_classes_are_objects_you_can_define_singleton_methods_on_them_too
-    assert_equal __, Dog2.wag
+    assert_equal :class_level_wag, Dog2.wag
   end
 
   def test_class_methods_are_independent_of_instance_methods
     fido = Dog2.new
-    assert_equal __, fido.wag
-    assert_equal __, Dog2.wag
+    assert_equal :instance_level_wag, fido.wag
+    assert_equal :class_level_wag, Dog2.wag
   end
 
   # ------------------------------------------------------------------
@@ -81,20 +83,23 @@ class AboutClassMethods < EdgeCase::Koan
   def test_classes_and_instances_do_not_share_instance_variables
     fido = Dog.new
     fido.name = "Fido"
-    assert_equal __, fido.name
-    assert_equal __, Dog.name
+    assert_equal "Fido", fido.name
+    assert_equal nil, Dog.name
   end
 
   # ------------------------------------------------------------------
 
   class Dog
+    # I've seen class methods written as 
+    # def self.a_class_method
+    # This would be the same thing then right?
     def Dog.a_class_method
       :dogs_class_method
     end
   end
 
   def test_you_can_define_class_methods_inside_the_class
-    assert_equal __, Dog.a_class_method
+    assert_equal :dogs_class_method, Dog.a_class_method
   end
       
 
@@ -103,9 +108,10 @@ class AboutClassMethods < EdgeCase::Koan
   LastExpressionInClassStatement = class Dog
                                      21
                                    end
-  
+  # Not only does every method in Ruby return a value (and not just 
+  # a Class) but every line executed has a return value.
   def test_class_statements_return_the_value_of_their_last_expression
-    assert_equal __, LastExpressionInClassStatement
+    assert_equal 21, LastExpressionInClassStatement
   end
 
   # ------------------------------------------------------------------
@@ -115,19 +121,22 @@ class AboutClassMethods < EdgeCase::Koan
                                end
 
   def test_self_while_inside_class_is_class_object_not_instance
-    assert_equal __, Dog == SelfInsideOfClassStatement
+    assert_equal true, Dog == SelfInsideOfClassStatement
   end
 
   # ------------------------------------------------------------------
 
   class Dog
+    # Yup, in reference to test_you_can_define_class_methods_inside_the_class 
+    # you can write class method the same way using "self." in place
+    # of the class name.
     def self.class_method2
       :another_way_to_write_class_methods
     end
   end
 
   def test_you_can_use_self_instead_of_an_explicit_reference_to_dog
-    assert_equal __, Dog.class_method2
+    assert_equal :another_way_to_write_class_methods, Dog.class_method2
   end
 
   # ------------------------------------------------------------------
@@ -141,7 +150,7 @@ class AboutClassMethods < EdgeCase::Koan
   end
 
   def test_heres_still_another_way_to_write_class_methods
-    assert_equal __, Dog.another_class_method
+    assert_equal :still_another_way, Dog.another_class_method
   end
 
   # THINK ABOUT IT:
@@ -159,12 +168,18 @@ class AboutClassMethods < EdgeCase::Koan
   #
   # Which do you prefer and why?
   # Are there times you might prefer one over the other?
-
+  # 
+  # Personally, I prefer the first. It's really weird for me to have to 
+  # write another class definition like that. While I guess it could 
+  # make sense the former is clearer to me. I like to define my class 
+  # methods separately from my instance ones which also helps make that 
+  # difference clear so I'm not concerned about def statements looking 
+  # the same. I just don't find the latter very clear initially.
   # ------------------------------------------------------------------
 
   def test_heres_an_easy_way_to_call_class_methods_from_instance_methods
     fido = Dog.new
-    assert_equal __, fido.class.another_class_method
+    assert_equal :still_another_way, fido.class.another_class_method
   end
 
 end
