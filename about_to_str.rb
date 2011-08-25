981a2a404ce3978d13827700f411b369c54eb006
Javier Julio <JJfutbol@gmail.com>
2011-08-25 01:23:17 -0400

    Completed last koan!

--- about_to_str.rb
+++ about_to_str.rb
@@ -10,11 +10,11 @@ class AboutToStr < EdgeCase::Koan
 
   def test_to_s_returns_a_string_representation
     not_like_a_string = CanNotBeTreatedAsString.new
-    assert_equal __, not_like_a_string.to_s
+    assert_equal "non-string-like", not_like_a_string.to_s
   end
 
   def test_normally_objects_cannot_be_used_where_strings_are_expected
-    assert_raise(___) do
+    assert_raise(TypeError) do
       File.exist?(CanNotBeTreatedAsString.new)
     end
   end
@@ -33,11 +33,17 @@ class AboutToStr < EdgeCase::Koan
 
   def test_to_str_also_returns_a_string_representation
     like_a_string = CanBeTreatedAsString.new
-    assert_equal __, like_a_string.to_str
+    assert_equal "string-like", like_a_string.to_str
   end
-
+  
+  # I thought that the to_s method was just a simple toString method 
+  # but apparently its not a strict one. Ruby supports a to_str method 
+  # but the difference isn't always so clear. The to_s isn't a strict
+  # conversion but to_str is. So only use to_str when you know that 
+  # object can be used everywhere a String can.
+  # http://briancarper.net/blog/98/
   def test_to_str_allows_objects_to_be_treated_as_strings
-    assert_equal __, File.exist?(CanBeTreatedAsString.new)
+    assert_equal false, File.exist?(CanBeTreatedAsString.new)
   end
 
   # ------------------------------------------------------------------
@@ -48,7 +54,7 @@ class AboutToStr < EdgeCase::Koan
   end
 
   def test_user_defined_code_can_check_for_to_str
-    assert_equal __, acts_like_a_string?(CanNotBeTreatedAsString.new)
-    assert_equal __,  acts_like_a_string?(CanBeTreatedAsString.new)
+    assert_equal false, acts_like_a_string?(CanNotBeTreatedAsString.new)
+    assert_equal true,  acts_like_a_string?(CanBeTreatedAsString.new)
   end
 end
